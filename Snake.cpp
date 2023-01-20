544ad708486f1c3bf00d7bef5b8693565bbb3053
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2023-01-19 19:51:31 -0600

    Se dibuja la serpiente

--- Snake.cpp
+++ Snake.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <conio.h>
 #include <windows.h>
+#include <dos.h>
+
+#define MAXSNKSIZE 100
 
 using  namespace std;
 
+/* A function that allows you to move the cursor to a specific position*/
 HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 COORD CursorPosition;
 
+/**
+ * It sets the cursor position to the x and y coordinates you specify.
+ * @param x The x coordinate of the cursor.
+ * @param y The y coordinate of the cursor.
+ **/
 void gotoxy(int x, int y){
     CursorPosition.X = x;
     CursorPosition.Y = y;
     SetConsoleCursorPosition(console,CursorPosition);
 }
 
+/* It's a class that represents a point in a 2D space */
 class Point{
     private:
         int x,y;
@@ -34,16 +44,16 @@ class Point{
         int GetY(){
             return y;
         }
-        void MUp(){
-            y--;
-        }
-        void MDown(){
+        void TurnUp(){
             y++;
         }
-        void MLeft(){
+        void TurnDown(){
+            y--;
+        }
+        void TurnLeft(){
             x--;
         }
-        void MRight(){
+        void TurnRight(){
             x++;
         }
         void Draw(){
@@ -54,14 +64,102 @@ class Point{
             gotoxy(x,y);
             cout<<" ";
         }
+        void CopyPos(Point * p){
+            p->x=x;
+            p->y=y;
+        }
         void Debug(){
             cout<<"("<<x<<","<<y<<")";
         }
 };
 
+/* It's a class that represents a snake */
+class Snake{
+    private:
+        Point * cell[MAXSNKSIZE];//Array que representa a la serpiente
+        int size;//Tamano actual de la serpiente
+        int dir;//Dirección actual de la serpiente
+        Point fruit;
+    /**
+     * The snake moves by making the body follow the head, and the head moves by changing the direction of the head.
+     * @param x The x coordinate of the cell
+     * @param y The y-coordinate of the upper-left corner of the rectangle to be filled.
+     */
+    public:
+        Snake(){
+            size=1;//Tamano por defecto
+            cell[0]=new Point(20,20);//Posicion por defecto (20,20)
+            for(int i=1;i<MAXSNKSIZE;i++){
+                cell[i]=NULL;
+            }
+        }
+        void AddCell(int x,int y){
+            cell[size++] = new Point(x,y);
+        }
+        void TurnDown(){
+            dir = 72;//Mover arriba (flecha)
+        }
+        void TurnUp(){
+            dir = 80;//Mover abajo (flecha)
+        }
+        void TurnLeft(){
+            dir = 75;//Mover izquierda (flecha)
+        }
+        void TurnRight(){
+            dir = 77;//Mover derecha (flecha)
+        }
+        void Move(){
+            //Hacer que el cuerpo siga a la cabeza
+            for(int i=size-1;i>0;i--){
+                cell[i]->CopyPos(cell[i-1]);
+            }
+
+            //Cabeza de la serpiente
+            switch(dir){
+                case 72:
+                    cell[0]->TurnDown();
+                    break;
+                case 75:
+                    cell[0]->TurnLeft();
+                    break;
+                case 77:
+                    cell[0]->TurnRight();
+                    break;
+                case 80:
+                    cell[0]->TurnUp();
+                    break;
+            }
+
+            //Dibujar la serpiente
+            for(int i=0;i<size;i++)
+                cell[i]->Draw();
+            Sleep(200);
+        }
+};
+
 int main(){
-    Point p(5,20);
-    p.Draw();
+    //Probamos
+    Snake snake;
+    char op='l';
+    do{
+        if(kbhit())
+            op=getch();
+        switch(op){
+            case 72:
+                snake.TurnDown();
+                break;
+            case 75:
+                snake.TurnLeft();
+                break;
+            case 77:
+                snake.TurnRight();
+                break;
+            case 80:
+                snake.TurnUp();
+                break;
+        }
+        snake.Move();
+    }while(op!='e');
 
     return 0;
 }
